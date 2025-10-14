29c9e6f94bf022b53a696fdd9c6fe0155549b564
Hari vardhana shubhash <Hari.Ponnamanda@valuemomentum.com>
2025-10-14 17:46:48 +0530

    Fix API key assignment to use environment variable

--- helpers/config.py
+++ helpers/config.py
@@ -3,6 +3,5 @@ from pathlib import Path
 import os
 
 load_dotenv()  # loads variables from .env into os.environ
-# api = os.environ["gemini_API3"]
-api="AIzaSyDiB4eEW2OLHKyAIQaBHfsGPaEnCwCeLH4"
+api = os.environ["gemini_API"]
 
